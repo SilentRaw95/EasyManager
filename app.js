1e15a6122e20066a4d2da757a19f621c7cc20e0e
Miguel Alejandro Guevara Mendoza <mike.gm.117@gmail.com>
2019-10-26 17:49:50 -0500

    avance en el ingreso de productos

--- app.js
+++ app.js
@@ -5,33 +5,46 @@ var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + '/public'));
 
 // conexion a mongodb
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
 mongoose.connect("mongodb://localhost:27017/node-demo");
-var nameSchema = new mongoose.Schema({
-  firstName: String,
-  lastName: String
+
+// schemas
+var productSchema = new mongoose.Schema({
+  name: String,
+  stock: Number,
+  price: Number,
+  // categories: [String]
 });
-var User = mongoose.model("User", nameSchema);
+var Product = mongoose.model("products", productSchema);
+
+// rutas
+// pagina de inicio
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html");
+});
+
+// add product
+app.get("/addProduct", (req, res) => {
+  res.sendFile(__dirname + "/views/addProduct.html");
+});
 
 // apis
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/index.html");
-});
-
-app.post("/addname", (req, res) => {
-  var myData = new User(req.body);
+app.post("/addProduct", (req, res) => {
+  var myData = new Product(req.body);
   myData.save()
     .then(item => {
-      res.send("Name saved to database");
+      res.send("product saved to database");
     })
     .catch(err => {
       res.status(400).send("Unable to save to database");
     });
 });
 
+// logs
 app.listen(port, () => {
   console.log("Server listening on port " + port);
 });
