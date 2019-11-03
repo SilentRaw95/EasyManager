ed72d967c507dc4cb1b42370091ddaa970b2b18d
Miguel Alejandro Guevara Mendoza <mike.gm.117@gmail.com>
2019-11-02 20:51:13 -0600

    pantalla añadir usuario

--- app.js
+++ app.js
@@ -10,21 +10,35 @@ app.use(express.static(__dirname + '/public'));
 // conexion a mongodb
 var mongoose = require("mongoose");
 mongoose.Promise = global.Promise;
-mongoose.connect("mongodb://localhost:27017/node-demo");
+mongoose.connect("mongodb://localhost:27017/easy-manager");
 
 // schemas
+var userSchema = new mongoose.Schema({
+  first_name: String,
+  last_name: String,
+  username: String,
+  password: String,
+  role: String
+});
+var UserDB = mongoose.model("user", userSchema);
+
 var productSchema = new mongoose.Schema({
   name: String,
   stock: Number,
   price: Number,
-  // categories: [String]
+  categories: [String]
 });
-var Product = mongoose.model("products", productSchema);
+var ProductDB = mongoose.model("products", productSchema);
 
 // rutas
 // pagina de inicio
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
+});
+
+// add user
+app.get("/addUser", (req, res) => {
+  res.sendFile(__dirname + "/views/addUser.html");
 });
 
 // add product
@@ -34,10 +48,23 @@ app.get("/addProduct", (req, res) => {
 
 // apis
 app.post("/addProduct", (req, res) => {
-  var myData = new Product(req.body);
+  req.body.categories = req.body.categories.split(',')
+
+  var myData = new ProductDB(req.body);
   myData.save()
     .then(item => {
-      res.send("product saved to database");
+      res.redirect("/addProduct")
+    })
+    .catch(err => {
+      res.status(400).send("Unable to save to database");
+    });
+});
+
+app.post("/addUser", (req, res) => {
+  var myData = new UserDB(req.body);
+  myData.save()
+    .then(item => {
+      res.redirect("/addUser")
     })
     .catch(err => {
       res.status(400).send("Unable to save to database");
